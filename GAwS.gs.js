f3977e796ce1f350931bd09bd3a4a336dc7156f9
abetomo <abe@enzou.tokyo>
2017-12-12 15:40:23 +0900

    Added method of synchronous invocation of Lambda
    The original method was dedicated asynchronously.

--- GAwS.gs.js
+++ GAwS.gs.js
@@ -109,13 +109,25 @@ var AWS = (function() {
       });
     },
 
-    lambda: function(region, action, method, functionName, payload) {
+    lambdaInvokeAsync: function(region, method, functionName, payload) {
       return this.request({
         service: 'lambda',
         region: region,
         method: method,
         payload: payload,
-        uri: '/2014-11-13/functions/' + functionName + '/' + action + '/',
+        uri: '/2014-11-13/functions/' + functionName + '/invoke-async/',
+        host: 'lambda.' + region + '.amazonaws.com',
+        headersDateKey: 'X-Amz-Date'
+      });
+    },
+
+    lambdaInvoke: function(region, method, functionName, payload) {
+      return this.request({
+        service: 'lambda',
+        region: region,
+        method: method,
+        payload: payload,
+        uri: '/2015-03-31/functions/' + functionName + '/invocations',
         host: 'lambda.' + region + '.amazonaws.com',
         headersDateKey: 'X-Amz-Date'
       });
